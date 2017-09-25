82fb960b18e36b919e9d82c69e09b77f2de853a9
chriswebb09 <christopher.webb@flatironschool.com>
2017-09-24 23:42:27 -0400

    added save to album

--- CoreCamera/CoreCamera/Screens/Controllers/Album/AlbumCollectionViewController.swift
+++ CoreCamera/CoreCamera/Screens/Controllers/Album/AlbumCollectionViewController.swift
@@ -13,9 +13,11 @@ protocol AlbumCollectionViewControllerDelegate: class {
 }
 
 class AlbumCollectionViewController: UIViewController, Controller {
-   
+    
     var type: ControllerType = .album
+    
     var images: [UIImage] = []
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     weak var delegate: AlbumCollectionViewControllerDelegate?
@@ -24,17 +26,27 @@ class AlbumCollectionViewController: UIViewController, Controller {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
-        let barButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(navigateBack))
+        let barButton = UIBarButtonItem(title: "Camera", style: .plain, target: self, action: #selector(self.navigateBack))
         DispatchQueue.main.async {
-            self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Camera", style: .plain, target: self, action: #selector(self.navigateBack))
+            self.navigationItem.leftBarButtonItem = barButton
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        dump(images)
         collectionView.reloadData()
-        
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Done", message: "Your photos have been saved.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     
     @objc func navigateBack() {
@@ -44,17 +56,31 @@ class AlbumCollectionViewController: UIViewController, Controller {
 
 extension AlbumCollectionViewController: UICollectionViewDelegate {
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let image = images[indexPath.row]
+        let context = CIContext()
+        if let ciImage = image.ciImage, let cgImage = context.createCGImage(ciImage, from: ciImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            UIImageWriteToSavedPhotosAlbum(uiImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
 }
 
 extension AlbumCollectionViewController: UICollectionViewDataSource {
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! ImageCell
         cell.photoView.image = images[indexPath.row]
         return cell
     }
 }
+
+
